86341511bf62c576d9a8eb11af2a13f9981bd0a9
JasperWang <jasper.wang@unisco.com>
2023-10-17 09:34:22 +0800

    1

---
+++ addCommas.py
@@ -0,0 +1,19 @@
+import clipboard
+
+# 获取剪贴板内容
+clipboard_text = clipboard.paste()
+
+# 将剪贴板文本分割成行，并去除前后空格
+lines = [line.strip() for line in clipboard_text.splitlines()]
+
+# 在每一行前后添加逗号
+formatted_lines = ['{0},'.format(line) for line in lines]
+
+# 将格式化后的行合并成一个字符串，一行一项
+result = '\n'.join(formatted_lines)
+
+# 去除最后一行的逗号
+result = result.rstrip(',')
+
+# 将结果复制到剪贴板
+clipboard.copy(result)
---
+++ addQuotes.py
@@ -0,0 +1,16 @@
+import clipboard
+
+# 获取剪贴板内容
+clipboard_text = clipboard.paste()
+
+# 将剪贴板文本分割成行，并去除前后空格
+lines = [line.strip() for line in clipboard_text.splitlines()]
+
+# 在每一行前后添加双引号
+formatted_lines = ['"{0}"'.format(line) for line in lines]
+
+# 将格式化后的行合并成一个字符串，一行一项
+result = '\n'.join(formatted_lines)
+
+# 将结果复制到剪贴板
+clipboard.copy(result)
