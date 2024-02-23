948c5a0bceb450a80001b6e97f67682cf9c37fda
JasperWang <jasper.wang@unisco.com>
2024-02-23 17:07:46 +0800

    1

--- checkOutAnyWhere.py
+++ checkOutAnyWhere.py
@@ -23,19 +23,22 @@ repository_name = choiceRepo()
 branch_name = input("请输入分支名：")
 repo_path = f"C:/Me/Code/{repository_name}"
 repo = git.Repo(repo_path)
-repo.git.fetch()
 # 尝试切换分支
 try:
     repo.git.checkout(branch_name)
     print(f"Successfully checked out branch {branch_name}.")
 except git.GitCommandError:
-    # 如果切换分支失败，则将未提交的更改暂存起来
-    print("Failed to checkout branch. Stashing changes...")
-    repo.git.stash()
-
-    # 再次尝试切换分支
     try:
+        repo.git.fetch()
         repo.git.checkout(branch_name)
         print(f"Successfully checked out branch {branch_name}.")
     except git.GitCommandError as e:
-        print(f"Failed to checkout branch {branch_name}: {e}")
+        # 如果切换分支失败，则将未提交的更改暂存起来
+        print("Failed to checkout branch. Stashing changes...")
+        repo.git.stash()
+        # 再次尝试切换分支
+        try:
+            repo.git.checkout(branch_name)
+            print(f"Successfully checked out branch {branch_name}.")
+        except git.GitCommandError as e:
+            print(f"Failed to checkout branch {branch_name}: {e}")
